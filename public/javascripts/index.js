9f6959037e509a50c87c9a3aea7efa7cdfe7bd74
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2016-01-24 14:17:06 -0500

    menu changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,3 +5,13 @@ $(document).ready(function(){
 $(document).ready(function() {
     $('.modal-trigger').leanModal();
 });
+
+$(document).ready(function (){
+    $('.dropdown-button').dropdown();
+});
+
+$(document).ready(function (){
+    $('.collapsible').collapsible({
+        accordion : false
+    });
+});
