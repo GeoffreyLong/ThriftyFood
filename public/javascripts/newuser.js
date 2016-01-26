5cce6b161158d5e28ed31d8960a059050ea713c5
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-01-26 12:34:22 -0500

    Changed logic for new user

--- public/javascripts/newuser.js
+++ public/javascripts/newuser.js
@@ -1,49 +1,51 @@
 // TODO refactor and make cleaner
 // Also fix the username < 5 but still entering bug
 $(document).ready(function(){
-  $('button[type="submit"]').prop('disabled', true);
-  $('body').on('keyup', '.userfield', function(){
-    var check = $(this);
-    if (check.val().length >= 5 || check.val().length == 0){
-      check.removeClass('invalid');
+  $('body').on('keyup', 'input', function(){
+    var userName = null;
+    var pass = null;
+    var passCheck = null;
+    var submitBtn = null;
+    if ($(this).hasClass('seller')){
+      userName = $('#sellerUserField');
+      pass = $('#sellerPass');
+      passCheck = $('#sellerPassCheck');
+      submitBtn = $('#sellerSubmit'); 
     }
     else{
-      check.addClass('invalid');
+      userName = $('#buyerUserField');
+      pass = $('#buyerPass');
+      passCheck = $('#buyerPassCheck');
+      submitBtn = $('#buyerSubmit');
     }
-  }); 
-  $('body').on('keyup', '.pass', function(){
-    var check = $(this);
-    if (check.val().length >= 5 || check.val().length == 0){
-      check.removeClass('invalid');
-    }
-    else{
-      check.addClass('invalid');
-    }
-  }); 
-  $('body').on('keyup', '.passCheck', function(){
-    //TODO check if this value matches the corresponding .pass 
-    // If not then the underlining should be in red, else blue
-    var check = $(this);
-    var table = $(this).parent().parent().parent();
-    var orig = table.find('.pass');
-    var submit = table.find('button[type="submit"]');
-    var userfield = table.find('.userfield');
-    if (orig.val() == check.val() && orig.val().length >= 5){
-      orig.addClass('valid');
-      check.addClass('valid');
-      userfield.addClass('valid');
-      check.removeClass('invalid');
-      submit.prop('disabled', false);
+
+    if (userName.val().length >= 5){
+      userName.removeClass('invalid');
     }
     else{
-      orig.removeClass('valid');
-      check.removeClass('valid');
-      userfield.removeClass('valid');
-      check.addClass('invalid');
-      submit.prop('disabled', true);
+      userName.addClass('invalid');
     }
-    if (check.val().length == 0){
-      check.removeClass('invalid');
+
+    if (pass.val().length >= 5){
+      pass.removeClass('invalid');
     }
-  }); 
+    else{
+      pass.addClass('invalid');
+    }
+
+    if (pass.val() == passCheck.val() && pass.val().length >= 5){
+      passCheck.removeClass('invalid');
+      passCheck.addClass('valid');
+      pass.addClass('valid');
+      userName.addClass('valid');
+      submitBtn.prop('disabled', false);
+    }
+    else{
+      passCheck.addClass('invalid');
+      passCheck.removeClass('valid');
+      pass.removeClass('valid');
+      userName.removeClass('valid');
+      submitBtn.prop('disabled', true);
+    }
+  });
 });
