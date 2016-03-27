ae3727ae76a00d4d418b5f31cf516ed9fb72ede5
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2016-03-27 18:59:47 -0400

    added server side stripe charge processing.

--- routes/purchases.js
+++ routes/purchases.js
@@ -3,6 +3,23 @@ var router = express.Router();
 var stripe = require('stripe')('sk_test_an3Nezne8XguJAefiBJgNV63');
 
 router.post('/create', function(req, res) {
+  var stripeToken = req.body.stripeToken;
+
+  var charge = stripe.charges.create({
+    amount: 100, //in cents
+    currency: 'cad',
+    source: stripeToken,
+    description: 'test charge'
+  }, function (err, charge) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(charge)
+    }
+  });
+
+  //redirect to homepage, needs to go to different page
   res.redirect('/');
 });
 
