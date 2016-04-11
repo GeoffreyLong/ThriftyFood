eddf77011a2912cb831d1cc980178fedf6ac2d86
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2016-04-11 00:46:21 -0400

    some more changes for payments

--- public/javascripts/purchase.js
+++ public/javascripts/purchase.js
@@ -9,9 +9,12 @@ var stripeResponseHandler = function(status, response) {
     $form.find('button').prop('disabled', false);
   } else {
     // token contains id, last4, and card type
+    //var foodId = $form.find('.food-id');//.getElementById('foodId').innerHTML;
+    var foodId = '56b2ab18492664680f91b6c6';
     var token = response.id;
     // Insert the token into the form so it gets submitted to the server
     $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+    $form.append($('<input type="hidden" name="foodId" />').val(foodId));
     // and re-submit
     $form.get(0).submit();
   }
--- routes/purchases.js
+++ routes/purchases.js
@@ -1,26 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var stripe = require('stripe')('sk_test_an3Nezne8XguJAefiBJgNV63');
+var Foods = require('../models/foods');
+var Users = require('../models/users');
 
+//create a purchase, post request with charge_token, foodId (probably buyerId too)
 router.post('/create', function(req, res) {
   var stripeToken = req.body.stripeToken;
+  var foodId = req.body.foodId;
 
-  var charge = stripe.charges.create({
-    amount: 100, //in cents
-    currency: 'cad',
-    source: stripeToken,
-    description: 'test charge'
-  }, function (err, charge) {
-    if (err) {
-      console.log(err);
+  console.log(stripeToken);
+  console.log(foodId);
+
+  //find purchased food
+  Foods.findById(foodId, function (food_err, food) {
+    if (food_err) {
+      //TODO
+      console.log('failed to find requested food for id [' + foodId + ']');
+      res.render('/');
     }
-    else {
-      console.log(charge)
-    }
+
+    console.log(JSON.stringify(food));
+
+    //find seller
+    Users.findById(food.sellerId, function (seller_err, seller) {
+      if (seller_err) {
+        //TODO
+        console.log('failed to find seller for requested food');
+        console.log(JSON.stringify(food, null, 2))
+        res.render('/');
+      }
+      console.log(JSON.stringify(seller));
+
+      stripe.charges.create({
+        amount: food.price * 100, //in cents
+        currency: 'cad',
+        source: stripeToken, //credit card token created client side by stripeJS
+        description: 'test connect charge',
+        application_fee: 10
+      }, {
+        stripe_account: seller.seller.stripeId
+      }, function (err, charge) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log(charge)
+          }
+
+          //redirect to homepage, needs to go to different page
+          res.redirect('/');
+      });
+    });
   });
-
-  //redirect to homepage, needs to go to different page
-  res.redirect('/');
 });
 
 module.exports = router;
